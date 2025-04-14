f441c8ac1b9d43adb7020f8542a07329e2df291f
Bhavika Patel <20592563+patelbhavika46@users.noreply.github.com>
2025-04-14 14:01:28 -0400

    updated config file

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
+/** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
 module.exports = {
   reactStrictMode: false,
   images: {
     domains: ["avatars.githubusercontent.com"],
+    unoptimized: true, // Important for static export!
   },
-  output: "standalone",
+  // output: "export", // Enables static export for GitHub Pages
+  assetPrefix: isProd ? "/patelbhavika46.github.io/" : "",
+  basePath: isProd ? "/patelbhavika46.github.io" : "",
 };
