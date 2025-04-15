fe25eddbb3e1ca4f4f18c7fee05926017b99899e
Bhavika Patel <20592563+patelbhavika46@users.noreply.github.com>
2025-04-15 14:01:03 -0400

    trying to resolve deployment issue

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
-  reactStrictMode: false,
+  reactStrictMode: true,
+  trailingSlash: true,
+  basePath: isProd ? "/patelbhavika46.github.io" : "",
+  assetPrefix: isProd ? "/patelbhavika46.github.io/" : "",
   images: {
     domains: ["avatars.githubusercontent.com"],
   },
-  basePath: "/patelbhavika46.github.io",
-  assetPrefix: "/patelbhavika46.github.io/",
 };
 
 module.exports = nextConfig;
