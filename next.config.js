6034e9e90106a897dcffc60660a81e45e0b67d71
Bhavika Patel <20592563+patelbhavika46@users.noreply.github.com>
2025-05-21 14:28:27 -0400

    new build

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
+if (process.env.NODE_ENV === "production") {
+  console.log("You're in production mode!");
+}
 const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
-  basePath: isProd ? "/patelbhavika46.github.io" : "",
+  basePath: isProd ? "/patelbhavika46.github.io/" : "",
   assetPrefix: isProd ? "/patelbhavika46.github.io/" : "",
   images: {
     domains: ["avatars.githubusercontent.com"],
