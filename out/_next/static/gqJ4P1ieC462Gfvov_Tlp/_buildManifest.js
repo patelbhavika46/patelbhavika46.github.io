5b4b759bad51dfb4850ae592c9ba4650051bcd76
Bhavika Patel <20592563+patelbhavika46@users.noreply.github.com>
2025-05-21 15:27:56 -0400

    new installation

---
+++ out/_next/static/gqJ4P1ieC462Gfvov_Tlp/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-799a31a6bf6eee6d.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
